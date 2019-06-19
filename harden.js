48124060b92f3002f3fa3ae76915476f8d69ed2d
AlionaBusuioc <37703874+AlionaBusuioc@users.noreply.github.com>
2019-06-19 21:59:45 +0300

    Add files via upload

---
+++ harden.js
@@ -0,0 +1,10 @@
+
+    function positive(element){
+        if(element >=0)
+        return true;
+        else
+        return false;
+    }
+    alert (positive(-10))
+    //////////////////////
+    // console.log(data.filter(positive));
